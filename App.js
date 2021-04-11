6d2d8d6e2a81a6aa4caf74fb7146531398b21598
Robert Jordan <robertjordan@Briennas-iMac.local>
2021-04-11 17:25:19 +1000

    navigation to settings page added

--- App.js
+++ App.js
@@ -5,95 +5,49 @@
  * @format
  * @flow strict-local
  */
+import 'react-native-gesture-handler';
+import React from 'react';
+import {Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './components/HomeScreen';
+import SettingsScreen from './components/SettingsScreen';
+import { colors } from './Colors';
 
-import React, {useState} from 'react';
-
-import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
-import AVMute from './components/AVMuteButton';
-import AutoFadeModule from './components/AutoFadeModule';
-import {colors} from './Colors';
-import TwoChannelMixer from './components/TwoChannelMixer';
-import {initSocket} from './socket';
+const Stack = createStackNavigator();
 
 const App = () => {
-  const [mySocket, setMySocket] = useState(initSocket);
-
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.body}>
-        <SafeAreaView style={{flex: 1}}>
-          <View style={styles.container}>
-            <TwoChannelMixer socket={mySocket} />
-            <View style={styles.controllButtons}>
-              <View>
-                <AVMute title={'AV Mute'} socket={mySocket} />
-              </View>
-              <View>
-                <AutoFadeModule title={'Auto Fade'} socket={mySocket} />
-              </View>
-            </View>
-          </View>
-        </SafeAreaView>
-      </View>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.lightgray,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ 
+            title: 'Mix Control'
+            
+          }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{ title: 'Settings'
+         }}
+        />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: colors.grey,
-    flex: 1,
-
-    paddingHorizontal: 24,
-  },
-
-  container: {
-    flex: 1,
-    alignContent: 'center',
-    justifyContent: 'center',
-  },
-
-  inputList: {
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-  },
-
-  controllButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-
-    paddingVertical: 8,
-  },
-
-  title: {
-    fontSize: 34,
-    fontWeight: '700',
-    color: colors.yellow,
-    textAlign: 'center',
-    marginTop: 8,
-  },
-
-  buttonText: {
-    fontSize: 14,
-    fontWeight: '800',
-    color: colors.yellow,
-    textTransform: 'uppercase',
-  },
-
-  inputText: {
-    fontSize: 28,
-    fontWeight: '800',
-    color: colors.grey,
-    textTransform: 'uppercase',
-    textAlign: 'center',
-  },
-
-  test1: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'red',
-  },
-});
 
 export default App;
---
+++ components/HomeScreen.js
@@ -0,0 +1,107 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React, {useState} from 'react';
+ import {SafeAreaView, StyleSheet, View, Button, StatusBar} from 'react-native';
+ import AVMute from './AVMuteButton';
+ import AutoFadeModule from './AutoFadeModule';
+ import {colors} from '../Colors';
+ import TwoChannelMixer from './TwoChannelMixer';
+ import {initSocket} from '../socket';
+ 
+ export default HomeScreen = (props) => {
+   const [mySocket, setMySocket] = useState(initSocket);
+ 
+   React.useLayoutEffect(() => {
+    props.navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => props.navigation.navigate('Settings')} title="X" />
+      ),
+    });
+  }, [props.navigation]);
+
+   return (
+    <>
+       <StatusBar barStyle="light-content" />
+       <View style={styles.body}>
+         <SafeAreaView style={{flex: 1}}>
+           <View style={styles.container}>
+             <TwoChannelMixer socket={mySocket} />
+             <View style={styles.controllButtons}>
+               <View>
+                 <AVMute title={'AV Mute'} socket={mySocket} />
+               </View>
+               <View>
+                 <AutoFadeModule title={'Auto Fade'} socket={mySocket} />
+               </View>
+             </View>
+           </View>
+         </SafeAreaView>
+       </View>
+    </>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   body: {
+     backgroundColor: colors.grey,
+     flex: 1,
+ 
+     paddingHorizontal: 24,
+   },
+ 
+   container: {
+     flex: 1,
+     alignContent: 'center',
+     justifyContent: 'center',
+   },
+ 
+   inputList: {
+     flexDirection: 'column',
+     justifyContent: 'space-around',
+   },
+ 
+   controllButtons: {
+     flexDirection: 'row',
+     justifyContent: 'space-around',
+ 
+     paddingVertical: 8,
+   },
+ 
+   title: {
+     fontSize: 34,
+     fontWeight: '700',
+     color: colors.yellow,
+     textAlign: 'center',
+     marginTop: 8,
+   },
+ 
+   buttonText: {
+     fontSize: 14,
+     fontWeight: '800',
+     color: colors.yellow,
+     textTransform: 'uppercase',
+   },
+ 
+   inputText: {
+     fontSize: 28,
+     fontWeight: '800',
+     color: colors.grey,
+     textTransform: 'uppercase',
+     textAlign: 'center',
+   },
+ 
+   test1: {
+     height: 100,
+     width: 100,
+     backgroundColor: 'red',
+   },
+ });
+ 
+
+ 
